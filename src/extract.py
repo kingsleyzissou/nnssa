d463a3564144332e49d0da8cf5098e1ed058e0de
Gianluca <20079110@mail.wit.ie>
2021-03-02 15:43:01 +0000

    feat: labels to bars function

--- src/extract.py
+++ src/extract.py
@@ -1,13 +1,14 @@
 import numpy as np
 import librosa
-import pickle
 import pandas as pd
 from tqdm import tqdm
-from utils.utils import load_labels, extract_melspecs, normalized_melbands
+import pickle
+from utils.utils import load_labels, extract_melspecs, normalized_melbands, beats_to_bars
 
 SR = 22050
-SALAMI_LABEL_DIR = "datasets/salami/annotations/"
-HARMONIX_LABEL_DIR = "datasets/harmonix/segments/"
+HOP_SIZE = 1024
+SALAMI_LABEL_DIR = "data/annotations/salami/annotations/"
+HARMONIX_LABEL_DIR = "data/annotations/harmonix/segments/"
 
 def load_beat_annotations(sal_fifty):
     features = []
@@ -26,11 +27,12 @@ def load_beat_annotations(sal_fifty):
                 mel = extract_melspecs(mp3, n_mels=24, sr=SR)
                 _, beats = librosa.beat.beat_track(mp3, sr=SR)
                 sync = librosa.util.sync(mel, beats)
+                # bars = beats_to_bars(sync)
                 item = {
                     "id": file_name,
                     "beat_times": librosa.frames_to_time(beats),
                     "beat_frames": beats,
-                    "beat_count": sync.shape[1],
+                    "beats_count": sync.shape[1],
                     "melspec": normalized_melbands(sync),
                     "segments": annotations
                 }
@@ -46,36 +48,38 @@ def load_harmonix_annotations(file):
 def load_harmonix(harmonix):
     features = []
     labels = []
-    for item in tqdm(harmonix, total=len(harmonix)):
+    for _, item in tqdm(harmonix.iterrows(), total=len(harmonix)):
         """ iterate through dataframe"""
-        file_name = item
-        mel = np.load('downloads/melspecs/' + file_name + '-mel.npy')
-        beat_times = pd.read_table('datasets/harmonix/beats_and_downbeats/' + file_name + '.txt', header=None)[0]
-        beat_frames = librosa.time_to_frames(beat_times, sr=SR)
-        sync = librosa.util.sync(mel, beat_frames)   
-        ## we have two label options for SALAMI database
+        file_name = item["File"]
+        mel = np.load('data/melspecs/harmonix/' + file_name + '-mel.npy')
+        beat_times = pd.read_table('data/annotations/harmonix/beats_and_downbeats/' + file_name + '.txt', header=None)[0]
+        beat_frames = librosa.time_to_frames(beat_times, sr=SR, hop_length=HOP_SIZE)
+        sync = librosa.util.sync(mel, beat_frames)
+        bars = beats_to_bars(sync)
         file_one = HARMONIX_LABEL_DIR + file_name + ".txt"
         annotations = load_harmonix_annotations(file_one)
         labels.append(annotations)
         item = {
             "id": file_name,
+            "bpm": item["BPM"],
             "beat_times": beat_times,
             "beat_frames": beat_frames,
-            "beat_count": sync.shape[1],
-            "melspec": normalized_melbands(sync),
+            "beats_count": sync.shape[1],
+            "bars_count": bars.shape[0],
+            "melspec": bars,
             "segments": annotations
         }
         features.append(item)
     return features, labels
 
 
-def match_labels_to_beats_harmonix(feature_list, label_list):
+def match_labels_to_beats(feature_list, label_list):
     labels = []
     for f,l in zip(feature_list, label_list):
-        beats = f["beat_count"]
+        beats = f["beats_count"]
         label_vec = np.zeros(beats)
         beat_frames = f["beat_frames"]
-        segment_frames = librosa.time_to_frames(l, sr=SR)
+        segment_frames = librosa.time_to_frames(l, sr=SR, hop_length=HOP_SIZE)
         for segment in segment_frames:
             min = np.abs(beat_frames - segment)
             closest_beat = np.argmin(min)
@@ -84,19 +88,36 @@ def match_labels_to_beats_harmonix(feature_list, label_list):
         labels.append(label_vec)
     return labels
 
-
-
+def match_labels_to_bars(feature_list, label_list):
+    labels = []
+    for f,l in zip(feature_list, label_list):
+        bars = f["bars_count"]
+        label_vec = np.zeros(bars)
+        beat_frames = f["beat_frames"]
+        segment_frames = librosa.time_to_frames(l, sr=SR, hop_length=HOP_SIZE)
+        for segment in segment_frames:
+            min = np.abs(beat_frames - segment)
+            closest_beat = np.argmin(min)
+            closest_beat = int(closest_beat/4)
+            if (closest_beat < bars):
+                label_vec[closest_beat] = 1
+        labels.append(label_vec)
+    return labels
 
 if __name__ == "__main__":
-    # harmonix = pd.read_csv("datasets/harmonix/metadata.csv")
-    songs = pickle.load(open('dumps/sal_fifty/salami_fifty.p', 'rb'))
+    harmonix = pd.read_csv("data/annotations/harmonix/metadata.csv")
+    # songs = pickle.load(open('dumps/sal_fifty/salami_fifty.p', 'rb'))
 
-    features, labels = load_beat_annotations(songs)
-    
-    labels = match_labels_to_beats_harmonix(features, labels)
+    four_four = harmonix[harmonix.columns][harmonix["Time Signature"] == "4|4"]
+    four_four = four_four[harmonix.columns][four_four["Genre"] == "Pop"]
+
+    songs = four_four.head(50)
+
+    features, labels = load_harmonix(songs)
+    labels = match_labels_to_bars(features, labels)
 
     features = pd.DataFrame(features)
     labels = pd.DataFrame(labels)
 
-    features.to_pickle("dumps/sal_fifty/features_normalized.p")
-    labels.to_pickle("dumps/sal_fifty/labels_normalized.p")
+    features.to_pickle("dumps/bars/features_normalized.p")
+    labels.to_pickle("dumps/bars/labels_normalized.p")
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import librosa
 
+CONTEXT_WINDOW = 17
+
 def filter_silence(frame):
     frame = frame[frame[1] != "Silence"]
     frame = frame[frame[1] != "End"]
@@ -49,8 +51,32 @@ def normalized_melbands(melspec):
         normalized_bands.append(normalize_band(band))
     return np.asarray(normalized_bands)
 
-
 def normalize_band(band):
     mean = band.mean(axis=0)
     std = band.std(axis=0)
     return (band - mean) / std
+
+def pad_melspec(n_mel_bands, padding, melspec):
+    return np.hstack((
+        np.zeros((n_mel_bands, padding)), 
+        melspec,
+        np.zeros((n_mel_bands, padding * 10))
+    ))
+
+def beats_to_bars(melspec):
+    beats = melspec.shape[1]
+    bars = int(beats / 4)
+    bars = bars if bars % 4 == 1 else bars + (5 - (bars % 4))
+    bands = np.zeros(shape=(bars, 4, melspec.shape[0], CONTEXT_WINDOW))
+    padded_melspec = pad_melspec(melspec.shape[0], 8, melspec)
+    for bar in range(bars):
+        b = []
+        for beat in range(bar * 4, bar * 4 + 4):
+            mel = padded_melspec[:, beat:beat + CONTEXT_WINDOW]
+            b.append(mel)
+            # print(mel.shape)
+            # mel = normalized_melbands(mel)
+            # bands[:, beat, :] = mel
+        # print(np.shape(b))
+        bands[bar, :, :, :] = b
+    return bands.swapaxes(1, 2)
