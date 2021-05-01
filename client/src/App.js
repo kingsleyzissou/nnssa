e8ec00f538a5bd9dbfc7d0403031ef158c782baa
Gianluca <gianlucazuccarelli@gmail.com>
2021-05-02 00:29:28 +0100

    feat: add navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import { Landing } from './views/Landing';
 import { Player } from './views/Player';
+import { Navigation } from './components/Navigation';
 
 const style = {
   'height': '100vh',
@@ -16,19 +17,22 @@ const style = {
 
 export default function App() {
   return (
-    <div style={style}>
-      <div style={{ display: 'block', width: '60%' }}>
-        <Router>
-          <Switch>
-            <Route path="/player">
-              <Player />
-            </Route>
-            <Route path="/">
-              <Landing />
-            </Route>
-          </Switch>
-        </Router>
+    <>
+      <Navigation />
+      <div style={style}>
+        <div style={{ display: 'block', width: '60%' }}>
+          <Router>
+            <Switch>
+              <Route path="/player">
+                <Player />
+              </Route>
+              <Route path="/">
+                <Landing />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
---
+++ client/src/components/Navigation/index.js
@@ -0,0 +1,96 @@
+import React, { useContext, useState } from "react";
+import classNames from "classnames";
+import {
+  Button,
+  Collapse,
+  InputGroup,
+  NavbarBrand,
+  Navbar,
+  NavLink,
+  Nav,
+  NavItem,
+  Container,
+  NavbarToggler,
+  CustomInput,
+} from "reactstrap";
+import { ThemeContext } from '../../contexts/ThemeContext';
+
+export function Navigation() {
+  const [collapseOpen, setcollapseOpen] = useState(false);
+  const [color, setcolor] = useState("navbar-transparent");
+  const { theme, changeTheme } = useContext(ThemeContext);
+
+  const toggleTheme = () => {
+    if (theme === '') {
+      changeTheme('white-content');
+      return;
+    }
+    changeTheme('');
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", updateColor);
+    return function cleanup() {
+      window.removeEventListener("resize", updateColor);
+    };
+  });
+  const updateColor = () => {
+    if (window.innerWidth < 993 && collapseOpen) {
+      setcolor("bg-white");
+    } else {
+      setcolor("navbar-transparent");
+    }
+  };
+  const toggleCollapse = () => {
+    if (collapseOpen) {
+      setcolor("navbar-transparent");
+    } else {
+      setcolor("bg-white");
+    }
+    setcollapseOpen(!collapseOpen);
+  };
+  return (
+    <>
+      <Navbar className={classNames("navbar-absolute", color)} expand="lg">
+        <Container fluid>
+          <div className="navbar-wrapper">
+            <NavbarBrand href="/">
+              <i className="tim-icons icon-sound-wave" />
+              &nbsp;&nbsp;NNSSA
+            </NavbarBrand>
+          </div>
+          <NavbarToggler onClick={toggleCollapse}>
+            <span className="navbar-toggler-bar navbar-kebab" />
+            <span className="navbar-toggler-bar navbar-kebab" />
+            <span className="navbar-toggler-bar navbar-kebab" />
+          </NavbarToggler>
+          <Collapse navbar isOpen={collapseOpen}>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="https://github.com/kingsleyzissou/nnssa">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="http://docs.nnssa.tech">
+                  Docs
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <Button color="link">
+                  <CustomInput
+                    id="switch"
+                    type="switch"
+                    checked={theme === ''}
+                    onChange={toggleTheme}
+                  />
+                </Button>
+              </NavItem>
+              <li className="separator d-lg-none" />
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
